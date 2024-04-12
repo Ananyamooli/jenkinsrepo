05f2fd4299ddd1a1216d804369af6a918b2e2caf
Ananyamooli <161895793+Ananyamooli@users.noreply.github.com>
2024-04-12 10:25:15 -0400

    0424

---
+++ 7.branch.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+    stages {
+        stage ('Build')
+        steps {
+            echo 'Build Pipeline'
+        }
+        stage ('Scan')
+        steps {
+            echo 'Scanning Pipelines'
+        }
+        stage ('Docker build')
+        steps {
+            echo 'Docker Pipelines'
+        }
+        stage ('Dev Deploy')
+        steps {
+            echo 'Dev pipelines'
+        }
+    }
+}
